0ab440f2beecddca09a818737264536ac4f4699d
arnoldherod <arnold.herod@gmail.com>
2018-10-11 00:39:36 +0700

    confusing but it works, lacking empty array

--- Exercise-13.js
+++ Exercise-13.js
@@ -1,48 +1,51 @@
 function countProfit(shoppers) {
-    let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
-                       ['Baju Zoro', 500000, 2],
-                       ['Sweater Uniklooh', 175000, 1]
-                     ];
+    let listBarang = [['Sepatu Stacattu', 1500000, 10],
+    ['Baju Zoro', 500000, 2],
+    ['Sweater Uniklooh', 175000, 1]
+    ];
     // you can only write your code here!
-  
-    
+
+    // loop to compare "product" from "shoppers" with "objRes.product"
+        // if SAME = objRes.shopper.push(shoppers[i]["name"])
+            // objRes.leftOver -= shoppers[i]["amount"]
+            // objRes.totalProfit = SUM of shoppers["amount"] * listBarang[i][1]
+        // result.push(objRes)
+    // END OF LOOP
+// return result
+
     var result = []
 
-    
-    for(var i = 0 ; i < listBarang.length ; i++){
-        var objRes = {}
-        objRes.product = listBarang[i][0]
-        result.push(objRes)
+    if(!shoppers){
+        return result
     }
-    
-    for(var j = 0 ; j < shoppers[0].length ; j++){
-        if(result[i]["product"] === listBarang[i][0]){ 
-            result[i]["shoppers"] = []
-            console.log(result)
-            if(result[i]["product"] === shoppers[0][i]["product"]){
-                result[i]["shoppers"].push(shoppers[0][i]["name"])
+    else{
+            for (var i = 0; i < listBarang.length; i++) {
+            var objRes = {}
+            objRes.product = listBarang[i][0]
+            objRes.shoppers = []
+            objRes.leftOver = listBarang[i][2]
+            objRes.totalProfit = 0
+
+            for (var key in objRes){
+                for (var iShoppers = 0 ; iShoppers < shoppers.length ; iShoppers++){
+                    if(objRes[key] === shoppers[iShoppers]["product"] && objRes.leftOver >= shoppers[iShoppers]["amount"]){
+                        objRes.shoppers.push(shoppers[iShoppers]["name"])
+                        objRes.leftOver -= shoppers[iShoppers]["amount"]
+                        objRes.totalProfit = listBarang[i][1] * (listBarang[i][2] - objRes.leftOver)// SUM OF shoppers["amount"]
+                    }
+                }
             }
+            result.push(objRes)
         }
     }
-    // console.log(result)
+    return result
 }
 
-
-//         if(shoppers[i]["product"] === result[i]["product"]){
-//             objRes.shoppers = shoppers[i]["name"]
-//         }
-//         objRes.leftOver = listBarang[i][2]
-//         objRes.totalProfit = listBarang[i][3]
-//         result.push(objRes)
-//     }
-//     return (result)
-// }
-
-  
-  // TEST CASES
-  console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 2},
-                            {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 3},
-                            {name: 'Rani', product: 'Sweater Uniklooh', amount: 2}]));
+// TEST CASES
+// console.log(countProfit(
+//     [{ name: 'Windi', product: 'Sepatu Stacattu', amount: 2 },
+//     { name: 'Vanessa', product: 'Sepatu Stacattu', amount: 3 },
+//     { name: 'Rani', product: 'Sweater Uniklooh', amount: 2 }]));
   //[ { product: 'Sepatu Stacattu',
   //   shoppers: [ 'Windi', 'Vanessa' ],
   //   leftOver: 5,
@@ -55,7 +58,7 @@ function countProfit(shoppers) {
   //   shoppers: [],
   //   leftOver: 1,
   //   totalProfit: 0 } ]
-  
+
 //   console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 8}, {name: 'Vanessa', product: 'Sepatu Stacattu', amount: 10}, {name: 'Rani', product: 'Sweater Uniklooh', amount: 1}, {name: 'Devi', product: 'Baju Zoro', amount: 1}, {name: 'Lisa', product: 'Baju Zoro', amount: 1}]));
   // [ { product: 'Sepatu Stacattu',
   //     shoppers: [ 'Windi' ],
@@ -84,4 +87,4 @@ function countProfit(shoppers) {
   //     leftOver: 1,
   //     totalProfit: 0 } ]
 
-//   console.log(countProfit([])); //[]
+  console.log(countProfit([])); //[]
